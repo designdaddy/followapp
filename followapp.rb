81b223701a23609501f0a4dc75a805ed949bd684
Chad Mefferd <cmefferd@gmail.com>
2011-02-25 23:15:47 -0600

    add function for follows view

--- followapp.rb
+++ followapp.rb
@@ -6,3 +6,13 @@ get '/' do
 
   erb :index
 end
+
+get '/follows' do 
+  @user1 = params[:user1]
+  @user2 = params[:user2]
+  
+  #not implemented yet
+  @following = is_following?(@user1, @user2)
+  
+  erb :follows
+end
